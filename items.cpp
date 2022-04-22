568dcc49ce0b578c86b3dcda63e9a096eb7a3a01
shaikhadil26 <m.adilshaikh26@gmail.com>
2022-04-22 11:11:47 +0530

    .

--- items.cpp
+++ items.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 class Categories{
@@ -32,7 +33,7 @@ class Categories{
     void displayList(){
         Categories *current = head;
         while(current != NULL){
-            cout<<current ->serialNo<<") "<<current ->name<<"\tRs. "<<current ->price;
+            cout<<current ->serialNo<<") "<<left<<setw(30)<<current ->name/*<<left<<setw(10)*/<<"Rs. "<<current ->price<<"\n";
             current = current->next;
         }
     }
@@ -41,7 +42,7 @@ class Categories{
 
 int main(){
     Categories fruits, vegetables, dairy, meats, snacks, bakery;
-    
+
     fruits.insert(1, "Apple", 150);
     fruits.insert(2, "Banana", 60);
     fruits.insert(3, "Grape", 25);
@@ -51,7 +52,7 @@ int main(){
     fruits.insert(7,"Dragon fruit",120);
     fruits.insert(8,"Orange",65);
     fruits.insert(9,"Mango",180);
-    fruits.insert(10,"Litchi",50);
+    
 
     vegetables.insert(1, "Potatoes", 40);
     vegetables.insert(2,"Tomatoes",60);
@@ -62,7 +63,7 @@ int main(){
     vegetables.insert(7,"Raddish",60);
     vegetables.insert(8,"Cucumber",40);
     vegetables.insert(9,"Cabbage",35);
-    vegetables.insert(10,"Eggplant",65);
+    
 
     dairy.insert(1,"Milk",65);
     dairy.insert(2,"Cottage Cheese",120);
@@ -73,7 +74,6 @@ int main(){
     dairy.insert(7,"Sour Cream",110);
     dairy.insert(8,"Yogurt",90);
     dairy.insert(9,"Cheese",120);
-    dairy.insert(10,"Whipped Cream",60);
 
     meats.insert(1,"Boneless Chicken Tenders", 500);
     meats.insert(2,"Chicken Livers", 350);
@@ -84,11 +84,6 @@ int main(){
     meats.insert(7,"Turkey Drumstick", 700);
     meats.insert(8,"Chicken Drumstick", 400);
     meats.insert(9,"Bacon", 250);
-    meats.insert(10,"Rack of Lamb", 1000);
-    meats.insert(11,"Sausage", 200);
-    meats.insert(12,"Ground Pork", 500);
-    meats.insert(13,"Whole Fryer Chicken", 2000);
-    meats.insert(14,"Pork Rib chops", 1000);
 
     snacks.insert(1, "Lays Chip Indian Masala", 20);
     snacks.insert(2, "Pringels Cream and Onions",100);
@@ -99,13 +94,6 @@ int main(){
     snacks.insert(7, "Puffcorn",20);
     snacks.insert(8, "Doritos",50);
     snacks.insert(9, "Hide and Seek",30);
-    snacks.insert(10, "Bourbon",50);
-    snacks.insert(11, "Jim Jam",30);
-    snacks.insert(12, "Peppy", 10);
-    snacks.insert(13, "Lays Classic Salt",30);
-    snacks.insert(14, "Bombay Mix",100);
-    snacks.insert(15, "Kurkure",20);
-    snacks.insert(16, "Too Yumm",30);
 
     bakery.insert(1,"French Baguette",200);
     bakery.insert(2,"Egg Twist Challah",350);
@@ -116,8 +104,45 @@ int main(){
     bakery.insert(7,"Bagel",50);
     bakery.insert(8,"Velvet Cupcake",50);
     bakery.insert(9,"Apple Pie",750);
-    bakery.insert(10,"New York Cheesecake",150);
-    bakery.insert(11,"Corn Muffins",75);
-    bakery.insert(12,"Lemon Tart",125);
-    bakery.insert(13,"Everything Bagel",80);
+
+    int ch2;
+    while(true){
+         cout<<"Select category for grocery: \n1. Fruits \n2.Vegetables  \n3.Dairy \n4.Meats \n5.Snacks  \n6.Bread & Bakery";
+                cin>>ch2;
+                switch (ch2)
+                {
+                case 1:
+                    cout<<"Fruits Menu :- \n";
+                    fruits.displayList();
+                    break;
+                case 2:
+                    cout<<"Vegetables Menu :- \n";
+                    vegetables.displayList();
+                    break;
+                case 3:
+                    cout<<"dairy Menu :- \n";
+                    dairy.displayList();
+                    break;
+                case 4:
+                    cout<<"Meats Menu :- \n";
+                    meats.displayList();
+                    break;
+                case 5:
+                    cout<<"Snacks Menu :- \n";
+                    snacks.displayList();
+                    break; 
+                case 6:
+                    cout<<"Bread & Bakery Menu :- \n";
+                    bakery.displayList();
+                    break;
+                case 7:
+                    cout<<"Fruits Menu :- ";
+                    fruits.displayList();
+                    break;
+
+                default:
+                    break;
+                }
+    }
+   
 }
